e160b927582e3b9fb39aa16fddfbe8ad3cc8b206
ayaad <ayaadalavi@gmail.com>
2024-09-13 09:56:34 +0000

    maded the footer

---
+++ src/app/(components)/sidebar/index.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { Menu } from "lucide-react";
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+        <div>LOGO</div>
+        <h1 className="font-extrabold text-2xl-">GilgitBazaar</h1>
+        <button
+          className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={() => {}}
+        >
+          <Menu className="w-4 h-4" />
+        </button>
+      </div>
+      {/* LINKS */}
+      <div className="flex-grow mt-8">{/* LINKS HERE */}</div>
+      {/* FOOTER */}
+      <div className="">
+        <p className="text-center text-xs text-gray-500">&copy; 2024 GBazar</p>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/app/dashBoardWrapper.tsx
+++ src/app/dashBoardWrapper.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Navbar from "@/app/(components)/Navbar";
+import Sidebar from "@/app/(components)/sidebar";
 const DashBoardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
-      Sidebar
+      <Sidebar />
       <main
         className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}
       >
